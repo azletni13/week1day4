1a60d493ea6d4002776aaf55c3f5afb1a381d7e3
Amna Zletni <azletni13@gmail.com>
2016-09-08 16:48:00 +0000

    added forEach in place of for loop

--- find_waldo.js
+++ find_waldo.js
@@ -1,14 +1,23 @@
-// The second argument/parameter is expected to be a function
+// // The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] == "Waldo") {
+//       found(arr);   // execute callback
+//     }
+//   }
+// }
+
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] == "Waldo") {
-      found(arr);   // execute callback
+  arr.forEach(function(name, index) {
+    if (name == "Waldo") {
+      found(index);
     }
-  }
+  });
 }
 
-function actionWhenFound(arr) {
-  console.log("Found him at " + arr.indexOf("Waldo"));
+function actionWhenFound(index) {
+  console.log(`Found him at ${index}`);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
